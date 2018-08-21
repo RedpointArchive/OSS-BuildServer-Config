d6f258c3bdc4a7329b090076235bc5e0cddc1078
June Rhodes <jrhodes@redpoint.games>
2018-08-21 14:45:20 +1000

    Limit what branches we build things for

--- config.groovy
+++ config.groovy
@@ -1,32 +1,3 @@
-organizationFolder('Protobuild') {
-  displayName('Protobuild')
-  triggers {
-      periodic(86400)
-  }
-  orphanedItemStrategy {
-    discardOldItems {
-    }
-  }
-  configure { folder ->
-    folder / 'navigators' / 'org.jenkinsci.plugins.github__branch__source.GitHubSCMNavigator'(plugin: 'github-branch-source@2.2.3') {
-      repoOwner('Protobuild')
-      credentialsId('redpoint-jenkins-oss-organisation-scanning')
-      traits {
-        'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
-          strategyId(1)
-        }
-        'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
-          strategyId(1)
-        }
-        'org.jenkinsci.plugins.github__branch__source.ForkPullRequestDiscoveryTrait' {
-          strategyId(1)
-          trust(class: 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait$TrustContributors')
-        }
-      }
-    }
-  }
-}
-
 organizationFolder('RedpointGames') {
   displayName('Redpoint Games')
   triggers {
@@ -44,12 +15,9 @@ organizationFolder('RedpointGames') {
         'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
           strategyId(1)
         }
-        'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
-          strategyId(1)
-        }
-        'org.jenkinsci.plugins.github__branch__source.ForkPullRequestDiscoveryTrait' {
-          strategyId(1)
-          trust(class: 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait$TrustContributors')
+        'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait'(plugin: 'scm-api@2.2.7') {
+          includes('master')
+          excludes('')
         }
       }
     }
@@ -73,12 +41,9 @@ organizationFolder('HiveMP') {
         'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
           strategyId(1)
         }
-        'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
-          strategyId(1)
-        }
-        'org.jenkinsci.plugins.github__branch__source.ForkPullRequestDiscoveryTrait' {
-          strategyId(1)
-          trust(class: 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait$TrustContributors')
+        'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait'(plugin: 'scm-api@2.2.7') {
+          includes('master')
+          excludes('')
         }
       }
     }
